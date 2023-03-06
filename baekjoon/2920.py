be9fd82edd9a25f40462df7a1ce206341a0a8c82
jangsumin <jsm0726@naver.com>
2023-03-06 22:51:45 +0900

    solve the 음계 problem in baekjoon

---
+++ baekjoon/2920.py
@@ -0,0 +1,10 @@
+# 음계
+
+arr = list(map(int, input().split()))
+
+if arr == sorted(arr):
+  print('ascending')
+elif arr == list(reversed(sorted(arr))):
+  print('descending')
+else:
+  print('mixed')
