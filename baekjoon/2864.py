97e545bc946f4bb3863b3784b54e8d279e8ceffa
jangsumin <jsm0726@naver.com>
2023-03-22 15:55:49 +0900

    solve the 5와 6의 차이 problem in baekjoon

---
+++ baekjoon/2864.py
@@ -0,0 +1,10 @@
+# 5와 6의 차이
+
+a, b = map(str, input().split())
+
+min_a = a.replace('6', '5')
+max_a = a.replace('5', '6')
+min_b = b.replace('6', '5')
+max_b = b.replace('5', '6')
+
+print(int(min_a) + int(min_b), int(max_a) + int(max_b))
