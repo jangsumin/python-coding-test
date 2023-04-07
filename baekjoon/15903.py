99908f9b6356b04356d1bc8edf19deb8896ed91a
jangsumin <jsm0726@naver.com>
2023-04-07 14:04:56 +0900

    solve 카드 합체 놀이 again by the different way

--- baekjoon/15903.py
+++ baekjoon/15903.py
@@ -3,8 +3,20 @@
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
-for i in range(m):
-  cards.sort()
-  cards[0], cards[1] = cards[0] + cards[1], cards[0] + cards[1]
+# for i in range(m):
+#   cards.sort()
+#   cards[0], cards[1] = cards[0] + cards[1], cards[0] + cards[1]
 
-print(sum(cards))
+# print(sum(cards))
+
+import heapq
+
+heap = cards[:]
+heapq.heapify(heap)
+for _ in range(m):
+  x = heapq.heappop(heap)
+  y = heapq.heappop(heap)
+  temp = x + y
+  heapq.heappush(heap, temp)
+  heapq.heappush(heap, temp)
+print(sum(heap))
